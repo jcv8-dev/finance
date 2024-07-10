f27a48ebfaa08a3b9bc91a1c0b025136bad864f8
JCVB <github@jcvb.de>
2024-07-10 15:26:01 +0200

    fix return

--- assets/scripts/readDB.php
+++ assets/scripts/readDB.php
@@ -591,7 +591,7 @@ function monatAusgabenOhneAnlagen($date){
     $sql = "select SUM(betrag) from buchungen join kategorie on buchungen.kategorieid = kategorie.id where not kategorie.kategorieBezeichnung = 'Anlage' and MONTH(datum) = $monat and YEAR(datum) = $jahr;";
     $result = db()->query($sql);
     foreach ($result as $key=>$value){
-        return $value;
+        return $value["SUM(betrag)"];
     }
     return 0;
 }
